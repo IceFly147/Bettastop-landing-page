d30d22bc4ace067fb44daa2351e354a1ae40072b
IceFly147 <100683747+IceFly147@users.noreply.github.com>
2022-08-01 17:39:01 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    if(window.scrollY >= 100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
